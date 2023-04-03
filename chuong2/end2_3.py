91ea9c26f592837a7e0400f5538f430d5bcedb7c
khunglonghoathach@gmail.com <email_cua_ban>
2023-04-03 22:31:06 +0700

    first commit

---
+++ chuong2/end2_3.py
@@ -0,0 +1,8 @@
+a=5
+b=10
+print("Nhap hai canh ke cua tam giac vuong:")
+input()
+input()
+import math
+c=math.sqrt(a**2+b**2)
+print("Canh ke thu nhat la: " + str(a) + ", canh ke thu hai: " + str(b) + ", Co canh huyen=",round(c,2))
---
+++ chuong2/end2_6.py
@@ -0,0 +1,9 @@
+ten=input('Ho ten: ')
+so_ngay_cong=int(input('So ngay cong: '))
+don_gia_ngay_cong=int(input('Don gia ngay cong: '))
+he_so_phu_cap=float(input('He so phu cap: '))
+tien_tam_ung=int(input('Tam ung: '))
+import math
+luong=don_gia_ngay_cong*so_ngay_cong*he_so_phu_cap
+thuc_linh=luong-tien_tam_ung
+print('Nhan vien ' +  str(ten) + ', Co tien luong= ' + str(luong) + ', Tam ung= ' + str(tien_tam_ung) + ' va Thuc Linh',round(thuc_linh,1))
