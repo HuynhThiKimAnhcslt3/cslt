2d7c4c9122ccefaffeadcee8f2be1bf517c13dd7
khunglonghoathach@gmail.com <email_cua_ban>
2023-04-10 22:00:17 +0700

    BTVeNhaC3Part1

--- chuong3/BTKetThucC3_3.py
+++ chuong3/BTKetThucC3_3.py
@@ -10,14 +10,14 @@ if ch==a or ch==b or ch==c or ch==d:
     if ch==a:
         print(str(x)+'+'+str(y)+'='+str(round(x+y,1)))
     elif ch==b:
-        print(str(x)+'+'+str(y)+'='+str(round(x-y,1)))
+        print(str(x)+'-'+str(y)+'='+str(round(x-y,1)))
     elif ch==c:
-        print(str(x)+'+'+str(y)+'='+str(round(x*y,1)))
+        print(str(x)+'*'+str(y)+'='+str(round(x*y,1)))
     elif ch==d:
         if y==0:
             print('Khong hop le')
         else:
-            print(str(x)+'+'+str(y)+'='+str(round(x/y,1)))
+            print(str(x)+'/'+str(y)+'='+str(round(x/y,1)))
 else:
     print('Khong hop le')
        
